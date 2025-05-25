e47fe5d0e2cc45326e7000de85f5e7e259fc2007
HoraHore <candrakiranaazzahraa@gmail.com>
2025-05-25 21:16:13 +0700

    ambil data perhitungan ke superadmin

--- resources/views/superadmin/perhitungan-smart/index.blade.php
+++ resources/views/superadmin/perhitungan-smart/index.blade.php
@@ -17,32 +17,37 @@
             </form>
         </div>
         <hr class="border-t-2 border-gray-300 mb-4 w-full">
+
         <!-- Tabel Data Calon Penerima dengan Scroll Horizontal -->
         <div class="overflow-x-auto h-96">
-            <table class="min-w-full table-auto border-collapse">
+            <table class="min-w-full table-auto border-collapse border border-gray-300">
                 <thead>
                     <tr class="bg-blue-800 text-white">
                         <th class="border px-4 py-2 text-left font-normal">No</th>
                         <th class="border px-4 py-2 text-left font-normal">Nama Calon Penerima</th>
                         <th class="border px-4 py-2 text-left font-normal">Beasiswa</th>
-                        <th class="border px-4 py-2 text-left font-normal">Kriteria 1</th>
-                        <th class="border px-4 py-2 text-left font-normal">Kriteria 2</th>
-                        <th class="border px-4 py-2 text-left font-normal">Kriteria 3</th>
-                        <th class="border px-4 py-2 text-left font-normal">Kriteria 4</th>
+                        @foreach ($headerKriteria as $namaKriteria)
+                            <th class="border px-4 py-2 text-center font-normal">{{ $namaKriteria }}</th>
+                        @endforeach
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($dataCalonPenerima as $data)
-                        <tr class="border-b">
-                            <td class="border px-4 py-2 text-left font-normal text-center">{{ $loop->iteration }}</td>
-                            <td class="border px-4 py-2 text-left font-normal">{{ $data->nama_calon_penerima }}</td>
-                            <td class="border px-4 py-2 text-left font-normal">{{ $data->pilihan_beasiswa }}</td>
-                            <td class="border px-4 py-2 text-left font-normal">{{ $data->nilai_kriteria1 }}</td>
-                            <td class="border px-4 py-2 text-left font-normal">{{ $data->nilai_kriteria2 }}</td>
-                            <td class="border px-4 py-2 text-left font-normal">{{ $data->nilai_kriteria3 }}</td>
-                            <td class="border px-4 py-2 text-left font-normal">{{ $data->nilai_kriteria4 }}</td>
+                    @forelse ($hasilPerhitungan as $i => $hasil)
+                        <tr class="border-b even:bg-gray-50 hover:bg-gray-100">
+                            <td class="border px-4 py-2 text-center">{{ $i + 1 }}</td>
+                            <td class="border px-4 py-2">{{ $hasil->calonPenerima->nama_calon_penerima }}</td>
+                            <td class="border px-4 py-2">{{ $hasil->jenisBeasiswa->nama }}</td>
+                            @foreach (array_keys($headerKriteria) as $idKriteria)
+                                <td class="border px-4 py-2 text-center">
+                                    {{ $hasil->nilai_kriteria[$idKriteria] ?? '-' }}
+                                </td>
+                            @endforeach
                         </tr>
-                    @endforeach
+                    @empty
+                        <tr>
+                            <td colspan="{{ 3 + count($headerKriteria) }}" class="text-center py-4">Belum ada data perhitungan.</td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </div>
